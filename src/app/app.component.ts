1e3f698c2bd5a3d575b6291db59e3a55f8e71a61
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-02-20 19:57:44 -0500

    clase 7 property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public name = 'Mikaela';
-  public age = 1;
+  name = 'Mikaela';
+  age = 1;
   nahomi = 'https://pbs.twimg.com/media/E0OjBEaWEAAxm2e.jpg';
+  btnDisabled = true;
+  byDefault = 'valor por defecto';
+  myage = 1;
+  img = 'https://cdn.yostagram.com/2023/02/nahomi-rojas-280.jpg';
+  person = {
+    name: 'nahomi',
+    age: 18,
+    avatar:
+      'https://i.pinimg.com/564x/4e/83/f5/4e83f57dff749d624e1093b3a6a65669.jpg',
+  };
 }
